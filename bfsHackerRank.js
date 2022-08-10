56254cff7d60b85a76b9b9682727a2924ca94dd5
marysonthego <marysonthego@gmail.com>
2022-08-10 11:33:03 -0600

    add link to problem

--- bfsHackerRank.js
+++ bfsHackerRank.js
@@ -1,4 +1,6 @@
 /*
+https://www.hackerrank.com/challenges/ctci-bfs-shortest-reach/problem
+
 Consider an undirected graph consisting of n nodes where each node is labeled from 1 to n and the edge between any two nodes is always of length 6. We define node s to be the starting position for a BFS. Given a graph, determine the distances from the start node to each of its descendants and return the list in node number order, ascending. If a node is disconnected, it's distance should be -1.
 
 For example, there are n - 6 nodes in the graph with a starting node s - 1. The list of
